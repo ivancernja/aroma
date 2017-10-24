8029788825c8735c82ad322c29391fa71a61707d
Ivan <ivancernja@gmail.com>
2017-10-24 14:20:44 +0200

    added plugins folder

---
+++ plugins/powr-social-media-icons/readme.txt
@@ -0,0 +1,169 @@
+===Social Media Icons===
+Contributors: powr
+Donate link: https://www.powr.io/
+Tags: social media, share icons, buttons, Facebook, Twitter, Instagram, LinkedIn, YouTube, email, profile, google plus, RSS
+Requires at least: 3.0
+Tested up to: 4.8
+Stable tag: 1.6.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Create fully customized social icons to get more followers on all of your social accounts.
+
+== Description ==
+= Choose your Plugin > Create POWr account > Add Social Media Icons to site =
+<br>
+✓ Grow your social media following on any platform with eye-catching custom icons.
+✓ Customize color, size, and shape of your social icons.
+✓ Add effects to make your icons pop and generate more clicks.
+<br>
+= Get Started: =
+Download, install, and activate Social Media Icons. Create and save your Plugin to your POWr account. Add Social Media Icons to your site.
+<br>
+= Free features: =
+* Official icons for Facebook, Instagram, Twitter, YouTube, and 50+ other social sites.
+* Fully customize the look of your icons: choose background and fill color, use square or round icons, adjust the size of icons, add hover effects, and display icons horizontally or vertically on your page.
+* Mobile responsive on any device.
+
+<br>
+= Upgrade to remove POWr branding and unlock Premium features, such as: =
+* Upload custom icons to link to any site.
+* Add animation effects to your icons to generate more clicks and grow your following more effectively.
+* Premium features in <a href="https://www.powr.io/plugins">all POWr apps</a>
+
+<br>
+= Why users love Social Media Icons: =
+“Don't even consider anything else. Not only is the product outstanding, heads & shoulders above anything else but the support is SUPERB. Use this app. I reiterate, don't even THINK about anything else. This is the best Social Media Icons with outstanding support!” - paralegalsperdiemh
+<br>
+** [See Social Media Icons in action on a user site](https://www.powr.io/demos/social-media-icons) **
+
+<br>
+= 🏆 Award Winning Customer Support 🏆 =
+If you have any questions or need help getting Social Media Icons up and running, we’re here to help! Please [visit our Help Center](https://www.powr.io/knowledge-base) for tips, support, and answers to common questions.
+
+
+
+== Installation ==
+Download, install, and activate POWr Social Media Icons.
+<br>
+<br>
+Then drop the plugin anywhere in your theme.
+<br>
+<br>
+Or add to a page/post with the POWr Icon in the WP text editor.
+<br>
+<br>
+Then visit your site to and click the settings icon to edit right in the page.
+<br>
+= <a href='https://www.powr.io/tutorials/how-to-add-social-media-icons-plugin-to-your-wordpress-site'>Click Here for 2 Minute Install Tutorial</a> =
+
+
+
+== Frequently Asked Questions ==
+
+= Do I need to create a POWr Account? =
+Yes, you need a POWr Account.  Your account allows you to create and save plugins, edit them in your live page, and use them on multiple different websites.
+
+= How do I add Social Media Icons to a page or blog post? =
+Create and save the plugin in your WP Dashboard.  Then copy and paste the unique shortcode into any page or post. You can continue editing the plugin on your live page, in your WP Dashboard, or by logging in to www.powr.io.
+
+= How do I add Social Media Icons to a theme? =
+In your WP Dashboard, select Appearance > Widgets from the left sidebar. Find Social Media Icons under Available Widgets and drag it into a sidebar or footer.  Then visit the live page to set up the plugin.
+
+= Can I add more than one Social Media Icons? =
+Yes, absolutely!  To clone an existing Social Media Icons, paste the exact same unique shortcode into another page or post.  To create a new version, select Social Media Icons from the sidebar in your WP Dashboard and click Create New.  You can then edit and save a new version, and paste the new unique shortcode into a page or post.
+
+= Will my users see the Edit button above Social Media Icons? =
+No! You only see the Edit button because you own the plugin. To temporarily hide it, click on your page and then type 'p + down arrow' (the letter p followed by the down arrow). You can show the icon again by typing 'p + up-arrow'.
+
+= I can't see the settings icon anymore, what happened? =
+You are just logged out of POWr.io. Visit <a href='https://www.powr.io'>www.powr.io</a> and log in to your account.  Then return to your website and refresh the page.
+
+= Is POWr Social Media Icons Free? =
+Yes, the basic version of Social Media Icons is completely free!
+
+= Why should I upgrade? =
+Upgrading gives you access to advanced features, removes the POWr logo, and gives you automatic priority support.
+
+
+
+== Screenshots ==
+1. Example
+2. Example
+3. Example
+4. Example
+5. Example
+
+
+== Upgrade Notice ==
+= 1.0 =
+First released version.
+
+= 1.1 =
+Improved SSL Support
+
+= 1.2 =
+Improved loading speed
+
+= 1.3 =
+Easily add POWr plugins right from the text editor.
+
+= 1.4 =
+Improved functionality within the wp text editor.
+
+= 1.5 =
+New ID system for easier identification of POWr Plugins. Settings panel in WP Admin allows easier creation and management of Plugins.
+
+= 1.5.1 =
+Fixed issue where POWr IDs get stripped
+
+= 1.5.2 =
+Bug fix for labels or IDs in shortcodes getting stripped
+
+= 1.6 =
+Easier to create and manage your POWr Plugins.
+
+= 1.6.2 =
+Bug Fixes (resolved some PHP notices seen by some users)
+
+= 1.6.3 =
+Supports latest plugins from POWr
+
+
+== Changelog ==
+= 1.0 =
+* First released version
+
+= 1.1 =
+* Improved SSL Support
+
+= 1.2 =
+* Improved loading speed
+
+= 1.3 =
+* Easily add POWr plugins right from the text editor.
+
+= 1.4 =
+* Improved functionality within the wp text editor.
+
+= 1.5 =
+* New ID system for easier identification of POWr Plugins.
+* Settings panel in WP Admin allows easier creation and management of Plugins.
+
+= 1.5.1 =
+* Fixed issue where POWr IDs get stripped
+
+= 1.5.2 =
+* Bug fix for labels or IDs in shortcodes getting stripped
+
+= 1.6 =
+* Local icon assets
+* Improved install workflow
+* Updated plugin list
+
+= 1.6.2 =
+* Fixed PHP Notices about undefined constants
+
+= 1.6.3 =
+* TinyMCE supports latest POWr Plugins
+
---
+++ plugins/under-construction-page/js/ucp-admin.js
@@ -0,0 +1,204 @@
+/*
+ * UnderConstructionPage
+ * Main backend JS
+ * (c) Web factory Ltd, 2015 - 2017
+ */
+
+
+jQuery(document).ready(function($) {
+  old_settings = $('#ucp_form').serialize();
+
+  // init tabs
+  $('#ucp_tabs').tabs({
+    activate: function(event, ui) {
+        Cookies.set('ucp_tabs_selected', $('#ucp_tabs').tabs('option', 'active'), { expires: 180 });
+    },
+    active: $('#ucp_tabs').tabs({ active: Cookies.get('ucp_tabs_selected') })
+  }).show();
+
+  // init select2
+  $('#whitelisted_users').select2({ 'placeholder': ucp.whitelisted_users_placeholder});
+
+  // autosize textareas
+  $.each($('textarea[data-autoresize]'), function() {
+    var offset = this.offsetHeight - this.clientHeight;
+
+    var resizeTextarea = function(el) {
+        $(el).css('height', 'auto').css('height', el.scrollHeight + offset + 2);
+    };
+    $(this).on('keyup input click', function() { resizeTextarea(this); }).removeAttr('data-autoresize');
+  });
+
+  // maybe init survey dialog
+  $('#features-survey-dialog').dialog({'dialogClass': 'wp-dialog ucp-dialog ucp-survey-dialog',
+                               'modal': 1,
+                               'resizable': false,
+                               'zIndex': 9999,
+                               'width': 705,
+                               'height': 'auto',
+                               'show': 'fade',
+                               'hide': 'fade',
+                               'open': function(event, ui) { ucp_fix_dialog_close(event, ui); },
+                               'close': function(event, ui) { },
+                               'autoOpen': ucp.open_survey,
+                               'closeOnEscape': true
+                              });
+
+
+  // turn questions into checkboxes
+  $('.question-wrapper').on('click', function(e) {
+    $('.question-wrapper').removeClass('selected');
+    $(this).addClass('selected');
+
+    e.preventDefault();
+    return false;
+  });
+
+
+  // dismiss survey forever
+  $('.dismiss-survey').on('click', function(e) {
+    $('#features-survey-dialog').dialog('close');
+
+    $.post(ajaxurl, { survey: $(this).data('survey'),
+                      _ajax_nonce: ucp.nonce_dismiss_survey,
+                      action: 'ucp_dismiss_survey'
+    });
+
+    e.preventDefault();
+    return false;
+  });
+
+
+  // submit and hide survey
+  $('.submit-survey').on('click', function(e) {
+    if ($('.question-wrapper.selected').length != 1) {
+      alert('Please choose your preferred autoresponder service.');
+      return false;
+    }
+
+    answers = '';
+    $('.question-wrapper.selected').each(function(i, el) {
+      answers += $(el).data('value') + ',';
+    });
+
+    $.post(ajaxurl, { survey: $(this).data('survey'),
+                      answers: answers,
+                      emailme: $('#features-survey-dialog #emailme:checked').val(),
+                      custom_answer: '',
+                      _ajax_nonce: ucp.nonce_submit_survey,
+                      action: 'ucp_submit_survey'
+    });
+
+    alert('Thank you for your time! We appriciate your input!');
+
+    $('#features-survey-dialog').dialog('close');
+    e.preventDefault();
+    return false;
+  });
+
+  // send support message
+  $('#send-support-message').on('click', function(e) {
+    e.preventDefault();
+    button = $(this);
+
+    if ($('#support_email').val().length < 5 || $('#support_email').is(':invalid')) {
+      alert('We need your email address, don\'t you think?');
+      $('#support_email').select().focus();
+      return false;
+    }
+
+    if ($('#support_message').val().length < 15) {
+      alert('An empty message won\'t do anybody any good.');
+      $('#support_message').select().focus();
+      return false;
+    }
+
+    button.addClass('loading');
+    $.post(ajaxurl, { support_email: $('#support_email').val(),
+                      support_message: $('#support_message').val(),
+                      support_info: $('#support_info:checked').val(),
+                      _ajax_nonce: ucp.nonce_submit_support_message,
+                      action: 'ucp_submit_support_message'},
+    function(data) {
+      if (data.success) {
+        alert('Message sent! Our agents will get back to you ASAP.');
+      } else {
+        alert(data.message);
+      }
+    }).fail(function() {
+      alert('Something is not right. Please reload the page and try again');
+    }).always(function() {
+      button.removeClass('loading');
+    });
+
+    return false;
+  });
+
+  // select theme via thumb
+  $('.ucp-thumb').on('click', function(e) {
+    e.preventDefault();
+
+    theme_id = $(this).data('theme-id');
+    $('.ucp-thumb').removeClass('active');
+    $(this).addClass('active');
+    $('#theme_id').val(theme_id);
+
+    return false;
+  });
+
+
+  // init datepicker
+  $('.datepicker').AnyTime_picker({ format: "%Y-%m-%d %H:%i", firstDOW: 1, earliest: new Date(), labelTitle: "Select the date &amp; time when construction mode will be disabled" } );
+
+
+  // reset/clear datepicker
+  $('.clear-datepicker').on('click', function(e) {
+    e.preventDefault();
+
+    $(this).prevAll('input.datepicker').val('');
+
+    return false;
+  });
+
+
+  // fix when opening datepicker
+  $('.show-datepicker').on('click', function(e) {
+    e.preventDefault();
+
+    $(this).prevAll('input.datepicker').focus();
+
+    return false;
+  });
+
+
+  // warning if there are unsaved changes when previewing
+  $(document).on('click', '#ucp_preview', function(e) {
+    if ($('#ucp_form').serialize() != old_settings) {
+      if (!confirm('There are unsaved changes that will not be visible in the preview. Please save changes first.\nContinue?')) {
+        e.preventDefault();
+        return false;
+      }
+    }
+
+    return true;
+  });
+
+
+  // helper for linking anchors in different tabs
+  $(document).on('click', '.change_tab', function(e) {
+    $('#ucp_tabs').tabs('option', 'active', $(this).data('tab'));
+
+    // get the link anchor and scroll to it
+    target = this.href.split('#')[1];
+    if (target) {
+      $.scrollTo('#' + target, 500, {offset: {top:-50, left:0}});
+    }
+  });
+}); // on ready
+
+
+function ucp_fix_dialog_close(event, ui) {
+  jQuery('.ui-widget-overlay').bind('click', function(){
+    jQuery('#' + event.target.id).dialog('close');
+  });
+} // ucp_fix_dialog_close
